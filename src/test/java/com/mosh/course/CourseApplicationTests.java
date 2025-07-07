fc40f4fdd9267d3e682cccd09bcac7ff7c4f085c
Erik Arutiunian <arutiunian.erik@seznam.cz>
2025-07-07 19:56:26 +0200

    Init

---
+++ src/test/java/com/mosh/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosh.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
