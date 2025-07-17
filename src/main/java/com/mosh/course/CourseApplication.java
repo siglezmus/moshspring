841c66d3c40e45d9aeb3431ab57858fb8b88f24e
Erik Arutiunian <arutiunian.erik@seznam.cz>
2025-07-17 13:35:25 +0200

    Prepare repo for part 2

--- src/main/java/com/mosh/course/CourseApplication.java
+++ src/main/java/com/mosh/course/CourseApplication.java
@@ -1,15 +1,6 @@
 package com.mosh.course;
 
-import com.mosh.course.Services.AddressService;
-import com.mosh.course.Services.ProductCategoryService;
-import com.mosh.course.Services.UserService;
-import com.mosh.course.models.User;
-import com.mosh.course.repositories.UserRepository;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class CourseApplication {
