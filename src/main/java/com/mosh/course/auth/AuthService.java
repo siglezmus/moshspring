e787ba98883154ae7235f1bdea43fa04156d5b6e
Erik Arutiunian <arutiunian.erik@seznam.cz>
2025-08-27 15:35:45 +0200

    Fix auth problem

--- src/main/java/com/mosh/course/auth/AuthService.java
+++ src/main/java/com/mosh/course/auth/AuthService.java
@@ -16,24 +16,16 @@ public class AuthService {
     public User getCurrentUser() {
         var auth = SecurityContextHolder.getContext().getAuthentication();
 
-        Object principal = auth.getPrincipal();
+        var principal = (JwtUserPrincipal) auth.getPrincipal();
 
-        if (principal instanceof JwtUserPrincipal jwtPrincipal) {
-            return userRepository.findById(jwtPrincipal.id()).orElse(null);
-        }
+        return userRepository.findById(principal.id()).orElse(null);
 
-        // If principal is anonymous or some other type
-        return null;
     }
 
     public Long getCurrentUserId(){
         var auth = SecurityContextHolder.getContext().getAuthentication();
+        var principal = (JwtUserPrincipal) auth.getPrincipal();
+        return principal.id();
 
-        Object principal = auth.getPrincipal();
-
-        if (principal instanceof JwtUserPrincipal jwtPrincipal) {
-            return jwtPrincipal.id();
-        }
-        return null;
     }
 }
