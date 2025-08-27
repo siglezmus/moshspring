7ad6a1fa309d4dc4edc05437a3f9a9613139c9a6
Erik Arutiunian <arutiunian.erik@seznam.cz>
2025-08-27 15:09:07 +0200

    Fix casting problem

--- src/main/java/com/mosh/course/auth/AuthService.java
+++ src/main/java/com/mosh/course/auth/AuthService.java
@@ -3,6 +3,7 @@ package com.mosh.course.auth;
 import com.mosh.course.users.User;
 import com.mosh.course.users.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,17 @@ public class AuthService {
 
     public User getCurrentUser(){
         var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var id = (Long) authentication.getPrincipal();
+        var principal = authentication.getPrincipal(); // this is a String ("1")
+
+        Long id;
+        if (principal instanceof String) {
+            id = Long.valueOf((String) principal);
+        } else if (principal instanceof Long) {
+            id = (Long) principal;
+        } else {
+            throw new BadCredentialsException("Unexpected principal type: " + principal.getClass());
+        }
+
         return userRepository.findById(id).orElse(null);
     }
 
